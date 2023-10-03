c69c9e5aa6f9a33919f78dab1190fe20a9ce7130
RiyaParte <parteriya21@gmail.com>
2023-10-02 23:10:46 -0400

    Update MinMaxCalculation.java

--- src/MinMaxCalculation.java
+++ src/MinMaxCalculation.java
@@ -1,3 +1,24 @@
 public class MinMaxCalculation {
-    
+
+    // Function to find the minimum value in an array
+    public static int findMin(int[] arr) {
+        int min = Integer.MAX_VALUE;
+        for (int num : arr) {
+            if (num < min) {
+                min = num;
+            }
+        }
+        return min;
+    }
+
+    // Function to find the maximum value in an array
+    public static int findMax(int[] arr) {
+        int max = Integer.MIN_VALUE;
+        for (int num : arr) {
+            if (num > max) {
+                max = num;
+            }
+        }
+        return max;
+    }
 }
